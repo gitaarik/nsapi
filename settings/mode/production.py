ddb3c5437c3fa38ff152ed0a30d828a8bc875eca
Rik <gitaarik@gmail.com>
2014-12-29 22:37:02 +0100

    added cache setting for production

--- settings/mode/production.py
+++ settings/mode/production.py
@@ -3,4 +3,10 @@ from django.views.decorators.cache import cache_page
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
 CACHE_FUNC = cache_page
